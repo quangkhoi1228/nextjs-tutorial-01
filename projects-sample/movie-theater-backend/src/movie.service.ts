953ba19858669ee5efbc3732d8345290ec632f1c
khoi tran <quangkhoi1228@gmail.com>
2025-05-30 21:24:02 +0700

    feat: add service

---
+++ projects-sample/movie-theater-backend/src/movie.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './movie.entity';
+import { CreateMovieDto } from './dto/createMovie.dto';
+import { UpdateMovieDto } from './dto/updateMovie.dto';
+
+@Injectable()
+export class MovieService {
+  private movies: Movie[] = [
+    {
+      id: 1,
+      name: 'The Dark Knight',
+      fromDate: new Date('2025-01-01'),
+      toDate: new Date('2025-01-01'),
+      is18Plus: false,
+    },
+    {
+      id: 2,
+      name: 'Tấm cám',
+      fromDate: new Date('2025-01-01'),
+      toDate: new Date('2025-01-01'),
+      is18Plus: true,
+    },
+  ];
+
+  // Lấy tất cả phim
+  getAllMovies(): Movie[] {
+    return this.movies;
+  }
+
+  getMovieById(id: number): Movie {
+    const movie = this.movies.find((movie) => movie.id === id);
+    if (!movie) {
+      throw new NotFoundException('Movie not found');
+    }
+    return movie;
+  }
+
+  createMovie(createMovieDto: CreateMovieDto): Movie {
+    const movie: Movie = {
+      id: this.movies.length + 1,
+      name: createMovieDto.name,
+      fromDate: createMovieDto.fromDate,
+      toDate: createMovieDto.toDate,
+      is18Plus: createMovieDto.is18Plus,
+    };
+    this.movies.push(movie);
+    return movie;
+  }
+
+  updateMovie(id: number, updateMovieDto: UpdateMovieDto): Movie {
+    let movie = this.getMovieById(id);
+    movie = { ...movie, ...updateMovieDto };
+    return movie;
+  }
+
+  deleteMovie(id: number): boolean {
+    this.movies = this.movies.filter((movie) => movie.id !== id);
+    return true;
+  }
+}
