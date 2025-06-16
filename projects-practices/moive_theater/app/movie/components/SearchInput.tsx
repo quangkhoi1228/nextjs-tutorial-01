1fe67e247ae69996a62aad6a68f360160b5d347e
khoi tran <quangkhoi1228@gmail.com>
2025-06-16 20:09:17 +0700

    feat: feedback homework class 06

--- projects-practices/moive_theater/app/movie/components/SearchInput.tsx
+++ projects-practices/moive_theater/app/movie/components/SearchInput.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function SearchInput() {
   return (
-    <div className="m-4">
-              <span className="text-gray-500  px-4">Search:</span>
+    <div className='m-4'>
+      <span className='text-gray-500  px-4'>Search:</span>
 
       <input
-        type="text"
-        className="border border-gray-300 rounded-md p-2 w-2xs"
+        type='text'
+        className='border border-gray-300 rounded-md p-2 w-2xs'
       />
-    
     </div>
-  )
+  );
 }
--- projects-practices/moive_theater/app/movie/components/TableMovie.tsx
+++ projects-practices/moive_theater/app/movie/components/TableMovie.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Info } from 'lucide-react'
+import React from 'react';
+import { Info } from 'lucide-react';
 
 export default function TableMovie() {
   const data = [
@@ -9,39 +9,55 @@ export default function TableMovie() {
     { id: 4, roomId: 4, name: 'Cinema room 4', seats: 60 },
     { id: 5, roomId: 5, name: 'Cinema room 5', seats: 60 },
     { id: 6, roomId: 6, name: 'Cinema room 6', seats: 60 },
-  ]
+  ];
+
+  const columns = [
+    { key: 'id', label: '#' },
+    { key: 'roomId', label: 'Cinema room ID' },
+    { key: 'name', label: 'Cinema room' },
+    { key: 'seats', label: 'Seat quantity' },
+    { key: 'detail', label: 'Seat detail' },
+  ];
 
   return (
-    <div className="px-4 py-6">
-      <table className="table-auto w-full border-collapse">
-        <thead className="bg-gray-50 border-b-4 border-gray-300">
+    <div className='px-4 py-6'>
+      <table className='table-auto w-full border-collapse'>
+        <thead className='  border-b-4 border-gray-300'>
           <tr>
-            <th className="px-4 py-2 text-left font-bold">#</th>
-            <th className="px-4 py-2 text-left font-bold">Cinema room ID</th>
-            <th className="px-4 py-2 text-left font-bold">Cinema room</th>
-            <th className="px-4 py-2 text-left font-bold">Seat quantity</th>
-            <th className="px-4 py-2 text-left font-bold">Seat detail</th>
+            {columns.map((col) => (
+              <th key={col.key} className='px-4 py-2 text-left font-bold '>
+                {col.label}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
           {data.map((item) => (
-            <tr key={item.id} className="odd:bg-gray-100 even:bg-white border-b-2 border-gray-200">
-              <td className="px-4 py-2">{item.id}</td>
-              <td className="px-4 py-2">{item.roomId}</td>
-              <td className="px-4 py-2">{item.name}</td>
-              <td className="px-4 py-2">{item.seats}</td>
-              <td className="px-4 py-2">
-                <div className="text-[#146db5] cursor-pointer flex items-center gap-2">
-                  <div className="w-4 h-4 bg-[#146db5] rounded-full flex items-center justify-center">
-                    <Info className="w-4 h-4 text-white" />
+            <tr
+              key={item.id}
+              className='odd:bg-gray-100 even:bg-white border-b-2 border-gray-200'
+            >
+              <td className='px-4 py-2'>{item.id}</td>
+              <td className='px-4 py-2'>{item.roomId}</td>
+              <td className='px-4 py-2'>{item.name}</td>
+              <td className='px-4 py-2'>{item.seats}</td>
+              <td className='px-4 py-2'>
+                <button
+                  className='text-[#146db5] cursor-pointer flex items-center gap-2'
+                  onClick={() => {
+                    console.log('Seat detail');
+                  }}
+                >
+                  <div className='w-4 h-4 bg-[#146db5] rounded-full flex items-center justify-center'>
+                    <Info className='w-4 h-4 text-white' />
                   </div>
                   <span>Seat detail</span>
-                </div>
+                </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
